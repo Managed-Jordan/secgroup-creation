0788ecd6af38e20a2131ab43ed6d2d65f49083fe
Managed-Jordan <45826455+Managed-Jordan@users.noreply.github.com>
2021-07-21 11:43:23 -0500

    Update and rename secgroup-update.ps1 to secgroup_update.ps1

--- secgroup_update.ps1
+++ secgroup_update.ps1
@@ -4,12 +4,12 @@
 #downloads and places the secgroup batch file
 " "
 "updating c:\netstrap\secgroup.bat..."
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Managed-Jordan/secgroup-creation/main/secgroup.txt' -OutFile 'C:\NetStrap\secgroup.bat'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Managed-Jordan/secgroup-creation/main/secgroup_users.bat' -OutFile 'C:\NetStrap\secgroup_users.bat'
 
 #runs updated file
 " "
 "Running secgroup.bat..."
-C:\NetStrap\secgroup.bat
+C:\NetStrap\secgroup_users.bat
 
 #sleep 2 seconds
 " "
