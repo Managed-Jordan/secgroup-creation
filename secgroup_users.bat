5950bdde545f2910055ec7e71eeccca94a0bec79
Managed-Jordan <45826455+Managed-Jordan@users.noreply.github.com>
2021-07-27 11:42:20 -0500

    Update secgroup_users.bat

--- secgroup_users.bat
+++ secgroup_users.bat
@@ -1,2 +1,4 @@
 net localgroup psacln | findstr "IWPD*" > C:\NetStrap\users.txt
 FOR /F %%A in (C:\NetStrap\users.txt) DO net localgroup secgroup %%A /add
+net stop W3SVC && net stop WAS
+net start W3SVC
