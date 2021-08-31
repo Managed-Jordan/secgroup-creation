6c8e89568b2ae5bba683951676bf8574224a6ff3
Managed-Jordan <45826455+Managed-Jordan@users.noreply.github.com>
2021-08-31 10:16:34 -0500

    Update secgroup_creation.ps1

--- secgroup_creation.ps1
+++ secgroup_creation.ps1
@@ -122,7 +122,7 @@ Set-Acl -Path $PATHNAME2 -AclObject $ACL2
 " "
 "Creating scheduled task in Windows..."
 $action = New-ScheduledTaskAction -Execute 'powershell' -Argument 'C:\NetStrap\secgroup_update.ps1'
-$trigger = New-ScheduledTaskTrigger -Weekly -At 4am
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 4am
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "Daily Secgroup Update" -Description "Created by Managed.com"
 
